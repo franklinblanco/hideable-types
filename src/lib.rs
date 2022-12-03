0d330c9d2c9ca575f33af7051056040d488c1d36
Franklin <franklinblanco@tutanota.com>
2022-12-03 18:25:53 -0400

    added vec impl

--- src/lib.rs
+++ src/lib.rs
@@ -31,3 +31,30 @@ pub trait Hideable {
     // /// Skip all fields except those that contain the specified attributes
     //fn hide_all_except_fields(attributes: Vec<String>);
 }
+/* 
+"primerobjeto": {
+    "vectordeobjetos": {
+        "1": {},
+        '2': {},
+        "3": {},
+    }
+}
+*/
+
+/// 
+impl<T: Hideable> Hideable for Vec<T> {
+    fn hide_fields(&self, attributes: Vec<String>) -> Result<serde_json::Map<String, serde_json::Value>, String> {
+        let mut values: Vec<Value> = Vec::new();
+        for element in self {
+            let key_value = match element.hide_fields(attributes.clone()) {
+                Ok(kv) => kv,
+                Err(e) => return Err(e),
+            };
+            values.push(key_value.into());
+        }
+        let mut map = Map::new();
+        map.insert("k".into(), values.into());
+        Ok(map)
+    }
+}
+
